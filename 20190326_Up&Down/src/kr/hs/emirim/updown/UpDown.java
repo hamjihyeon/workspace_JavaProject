dd0782d864b439770f43c20784e0a5a8e342e2a5
hamjihyeon <s2018w20@e-mirim.hs.kr>
2019-03-28 19:49:58 +0900

    add page project

--- 20190326_Up&Down/src/kr/hs/emirim/updown/UpDown.java
+++ 20190326_Up&Down/src/kr/hs/emirim/updown/UpDown.java
@@ -7,21 +7,21 @@ public class UpDown {
 
 	public static void main(String[] args) {
 		Random random = new Random();
-		int rNumber = random.nextInt(100);
-		rNumber += rNumber;
-		Scanner scanner = new Scanner(System.in);
+		int answer = random.nextInt(100) + 1;
+		System.out.println("컴퓨터가 생각하는 숫자를 입력하세요(1~100)");
 		while (true) {
-			int an = scanner.nextInt();
-			if (rNumber == an) {
-				System.out.println("끝");
-				break;
-			} else if (rNumber < an) {
-				System.out.println("DOWN");
-			} else {
+			Scanner scanner = new Scanner(System.in);
+			int player = scanner.nextInt();
+
+			if (answer > player) {
 				System.out.println("UP");
+			} else if (answer < player) {
+				System.out.println("Down");
+			} else {
+				System.out.println("정답입니다.");
+				break;
 			}
 		}
-
 	}
 
 }
