a72e1dcedd5f77a038d846ee1421e6baf89ca45f
x-note <x-note@DESKTOP-VCSI6S8>
2019-05-21 18:56:04 +0900

    add JRadioButton, ButtonGroup

---
+++ 20190430_GUI/src/GUITest3_JCheckBox.java
@@ -0,0 +1,33 @@
+import java.awt.Dimension;
+
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GUITest3_JCheckBox {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("창이름");
+		frame.setLocation(800, 300);
+		frame.setPreferredSize(new Dimension(300, 200));//선호하는 사이즈
+		//frame.setSize(300, 400);
+		
+		JPanel panel = new JPanel();
+		//JCheckBox 치킨
+		JCheckBox cbChicken = new JCheckBox("치킨");
+		//JCheckBox 피자
+		JCheckBox cbPizza = new JCheckBox("피자");
+		
+		panel.add(cbChicken);
+		panel.add(cbPizza);
+		frame.add(panel);
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
