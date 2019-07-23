75d01ca3922c06594ec3850f3cb949e5ffcc9eb4
hamjihyeon <gkawlgus01@naver.com>
2019-07-23 09:49:45 +0900

    ''->','로 바꾸기

---
+++ 20190723/src/ArrayCopy.java
@@ -0,0 +1,23 @@
+
+public class ArrayCopy {
+
+	static void ArrayCopy(char a[]) {
+		for (int i = 0; i < a.length; i++) 
+			if (a[i] == ' ') 
+				a[i] = ',';
+	}
+		static void AC(char a[]) {
+			for(int i = 0; i<a.length; i++) {
+				System.out.println(a[i]);
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		char c[] = {'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','l','e'};
+		AC(c);
+		ArrayCopy(c);
+		AC(c);
+	}
+
+}
