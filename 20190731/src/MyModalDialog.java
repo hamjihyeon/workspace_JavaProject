5d840a01580173cb96bff1f9a46abe5af0d92407
hamjihyeon <gkawlgus01@naver.com>
2019-07-31 09:43:53 +0900

    text에 글씨 넣고 끄기

---
+++ 20190731/src/MyModalDialog.java
@@ -0,0 +1,35 @@
+
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class MyModalDialog extends JDialog{
+		JTextField tf = new JTextField(10);
+		JButton okButton = new JButton("OK");
+		
+		public MyModalDialog(JFrame frame, String title) {
+			super(frame, title, true);
+			setLayout(new FlowLayout());
+			add(tf);
+			add(okButton);
+			setSize(200, 100);
+			
+			okButton.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					setVisible(false);
+				}
+			});
+		}
+		String getInput() {
+			if(tf.getText().length() == 0) return null;
+			else return tf.getText();
+		}
+}
